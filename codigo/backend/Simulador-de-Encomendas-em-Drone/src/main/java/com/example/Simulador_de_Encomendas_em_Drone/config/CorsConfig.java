6cbbdf38de78f7d5b77714a553ef63cdbb15464a
JosueGoulart01 <jcgreis@sga.pucminas.br>
2026-07-20 01:27:13 -0300

    fix: cors

--- codigo/backend/Simulador-de-Encomendas-em-Drone/src/main/java/com/example/Simulador_de_Encomendas_em_Drone/config/CorsConfig.java
+++ codigo/backend/Simulador-de-Encomendas-em-Drone/src/main/java/com/example/Simulador_de_Encomendas_em_Drone/config/CorsConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://simulador-de-encomendas-em-drone-frontend.onrender.com")
+                .allowedOriginPatterns("https://simulador-de-encomendas-em-drone-frontend.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
